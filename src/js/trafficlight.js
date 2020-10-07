ec1f2cf5aa43c4099b66e21a0aa22ba7a4ac991d
Ricky2297 <rickysgb@gmail.com>
2020-10-07 16:06:10 +0000

    Traffic light with Reac

---
+++ src/js/trafficlight.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export class Traficclight extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+	setColor = colorName => {
+		this.setState({ color: colorName });
+	};
+	render() {
+		return (
+			<div>
+				<div
+					id="tube"
+					className="container-fluid justify-content-center"
+				/>
+				<div
+					id="trafficc-parent"
+					className="container-fluid justify-content-center">
+					<div
+						className={
+							this.state.color == "red"
+								? "red active"
+								: "red inactive"
+						}
+						onClick={() => {
+							this.setColor("red");
+						}}
+					/>
+
+					<div
+						className={
+							this.state.color == "yellow"
+								? "yellow active"
+								: "yellow inactive"
+						}
+						onClick={() => {
+							this.setColor("yellow");
+						}}
+					/>
+					<div
+						className={
+							this.state.color == "green"
+								? "green active"
+								: "green inactive"
+						}
+						onClick={() => {
+							this.setColor("green");
+						}}
+					/>
+				</div>
+			</div>
+		);
+	}
+}
